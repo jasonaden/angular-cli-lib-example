97d0d1349d3d1d43b0f58f48dd4387fb3666ed28
Juri <juri.strumpflohner@gmail.com>
2017-12-01 11:51:56 +0100

    feat(step-4): compile to fesm5

---
+++ rollup-esm5.conf.js
@@ -0,0 +1,31 @@
+export default {
+  entry: 'tmp/esm5/ngx-tabs-libdemo.js',
+  dest: 'dist/esm5/ngx-tabs-libdemo.js',
+  format: 'es',
+  globals: {
+    // Angular dependencies
+    '@angular/animations': 'ng.animations',
+    '@angular/core': 'ng.core',
+    '@angular/common': 'ng.common',
+    '@angular/common/http': 'ng.common.http',
+    '@angular/cdk': 'ng.cdk',
+    '@angular/cdk/a11y': 'ng.cdk.a11y',
+    '@angular/cdk/accordion': 'ng.cdk.accordion',
+    '@angular/cdk/bidi': 'ng.cdk.bidi',
+    '@angular/cdk/coercion': 'ng.cdk.coercion',
+    '@angular/cdk/collections': 'ng.cdk.collections',
+    '@angular/cdk/keycodes': 'ng.cdk.keycodes',
+    '@angular/cdk/layout': 'ng.cdk.layout',
+    '@angular/cdk/observers': 'ng.cdk.observers',
+    '@angular/cdk/overlay': 'ng.cdk.overlay',
+    '@angular/cdk/platform': 'ng.cdk.platform',
+    '@angular/cdk/portal': 'ng.cdk.portal',
+    '@angular/cdk/rxjs': 'ng.cdk.rxjs',
+    '@angular/cdk/scrolling': 'ng.cdk.scrolling',
+    '@angular/cdk/stepper': 'ng.cdk.stepper',
+    '@angular/cdk/table': 'ng.cdk.table',
+    '@angular/forms': 'ng.forms',
+    '@angular/http': 'ng.http',
+    '@angular/router': 'ng.router'
+  }
+};
