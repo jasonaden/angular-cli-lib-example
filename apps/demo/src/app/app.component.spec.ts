11ef7fbdd39f42b85bbc89e8e16d4a48d2e33a46
Jason Aden <jasonaden@google.com>
2018-01-23 23:25:23 -0800

    step 0: use tabs lib in app component

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { NgxTabsLibModule } from 'ngx-tabs-lib';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -9,6 +10,7 @@ describe('AppComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
+        imports: [NgxTabsLibModule],
         declarations: [AppComponent]
       }).compileComponents();
     })
--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NxModule } from '@nrwl/nx';
+
+import {NgxTabsLibModule} from '@angular-cli-lib-example/ngx-tabs-lib';
 
 @NgModule({
-  imports: [BrowserModule, NxModule.forRoot()],
+  imports: [
+    BrowserModule,
+    NgxTabsLibModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
