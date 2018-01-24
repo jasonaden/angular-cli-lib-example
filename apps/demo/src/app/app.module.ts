20a8ee9901bdc1670c9d8f51be3840e1970a62d1
Jason Aden <jasonaden@google.com>
2018-01-23 23:40:48 -0800

    step 1: map library name in tsconfig

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {NgxTabsLibModule} from '@angular-cli-lib-example/ngx-tabs-lib';
+import {NgxTabsLibModule} from 'ngx-tabs-lib';
 
 @NgModule({
   imports: [
